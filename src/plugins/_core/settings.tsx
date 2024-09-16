f38d1c70d1882ec4bfd3cc2e240ee0dc2a972a68
falc0n2222 <92944974+falc0n2222@users.noreply.github.com>
2024-09-16 23:10:12 +0200

    Update settings.tsx

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -234,7 +234,7 @@ export default definePlugin({
     getInfoRows() {
         const { electronVersion, chromiumVersion, additionalInfo } = this;
 
-        const rows = [`Equicord ${gitHash}${additionalInfo}`];
+        const rows = [`Falcord ${gitHash}${additionalInfo}`];
 
         if (electronVersion) rows.push(`Electron ${electronVersion}`);
         if (chromiumVersion) rows.push(`Chromium ${chromiumVersion}`);
