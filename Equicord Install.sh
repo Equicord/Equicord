8d5cc94cd675f4c3b875b3e9a23f54f37251564d
PhoenixAceVFX <phoenixacevfx@trigon.systems>
2024-11-19 15:22:03 -0500

    Update Equicord Install.sh
    Pulled changed by Crxaw

--- Equicord Install.sh
+++ Equicord Install.sh
@@ -2,29 +2,50 @@
 set -e
 
 if [ "$(id -u)" -eq 0 ]; then
-    echo "Run me as normal user, not root!"
+    echo "Run me as a normal user, not root!"
     exit 1
 fi
 
-outfile=$(mktemp)
-trap 'rm -f "$outfile"' EXIT
+misc_dir="$HOME/misc" # Install to ~/misc suggested by thororen  --PhoenixAceVFX: This is alot better than what I could write even if I just edited a fork
+mkdir -p "$misc_dir"
 
-echo "Downloading Installer..."
+installer_path="$misc_dir/EquilotlCli-Linux"
+github_url="https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli-Linux"
 
-set -- "XDG_CONFIG_HOME=$XDG_CONFIG_HOME"
+echo "Checking if the installer needs updating..."
 
-curl -sS https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli-Linux \
-  --output "$outfile" \
-  --location
 
-chmod +x "$outfile"
+latest_modified=$(curl -sI "https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli-Linux" | grep -i "last-modified" | cut -d' ' -f2-)
+
+if [ -f "$installer_path" ]; then
+    # Get time of last modification 
+    local_modified=$(stat -c "%y" "$installer_path" | cut -d' ' -f1-2)
+
+# kinda shitty way to update but it kinda works ig 
+    if [ "$local_modified" = "$latest_modified" ]; then
+        echo "The installer is up-to-date."
+    else
+        echo "The installer is outdated. Downloading the latest version..."
+        curl -sSL "https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli-Linux" --output "$installer_path"
+        chmod +x "$installer_path"
+    fi
+else
+    echo "Installer not found. Downloading it..."
+    curl -sSL "https://github.com/Equicord/Equilotl/releases/latest/download/EquilotlCli-Linux" --output "$installer_path"
+    chmod +x "$installer_path"
+fi
+
 
 if command -v sudo >/dev/null; then
-  echo "Running with sudo"
-  sudo env "$@" "$outfile"
+    echo "Running installer with sudo..."
+    sudo "$installer_path"
 elif command -v doas >/dev/null; then
-  echo "Running with doas"
-  doas env "$@" "$outfile"
+    echo "Running installer with doas..."
+    doas "$installer_path"
 else
-  echo "Neither sudo nor doas were found. Please install either of them to proceed."
+    echo "Neither sudo nor doas were found. Please install one to proceed."
+    exit 1
 fi
+echo "Original script forked from Vencord"
+echo "Modified by PhoenixAceVFX for Equicord"
+echo "Modified by Crxaw for Updater System"
