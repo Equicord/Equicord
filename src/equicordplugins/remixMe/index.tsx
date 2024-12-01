334cdd3fe49f1e15a5e33b29ef9956ad2560f634
Krystal <username0458478374@gmail.com>
2024-12-01 16:27:49 -0500

    clean up

--- src/equicordplugins/remixMe/index.tsx
+++ src/equicordplugins/remixMe/index.tsx
@@ -7,7 +7,6 @@
 import { addPreSendListener, MessageExtra, MessageObject, removePreSendListener } from "@api/MessageEvents";
 import { EquicordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { Forms } from "@webpack/common";
 
 const handleMessage = (channelID: string, message: MessageObject, messageEx: MessageExtra) => messageEx.uploads && messageEx.uploads.forEach(att => (att as any).isRemix = true);
 
@@ -15,11 +14,6 @@ export default definePlugin({
     name: "RemixMe",
     description: "Turns every single message with attachment to have remix tag",
     authors: [EquicordDevs.kvba],
-    settingsAboutComponent: () => <>
-        <Forms.FormText className="remixme-warning">
-            We can't guarantee this plugin won't get you warned or banned.
-        </Forms.FormText>
-    </>,
     start: () => addPreSendListener(handleMessage),
     stop: () => removePreSendListener(handleMessage)
 });
