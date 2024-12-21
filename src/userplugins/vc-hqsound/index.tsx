aa756816ee3110b5af11b8ff159568b8cdd8ee7b
Chaython <chaython@live.ca>
2024-12-20 22:13:45 -0700

    Update index.tsx

--- src/userplugins/vc-hqsound/index.tsx
+++ src/userplugins/vc-hqsound/index.tsx
@@ -4,17 +4,37 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import definePlugin from "@utils/types";
+import definePlugin, { OptionType } from "@utils/types";
 import { useEffect } from "@webpack/common";
 import { Devs } from "@utils/constants";
+import { definePluginSettings } from "@api/Settings";
+
+const settings = definePluginSettings({
+    enableStereo: {
+        type: OptionType.BOOLEAN,
+        description: "Enable stereo audio",
+        default: true,
+        restartNeeded: true,
+    },
+    bitrate: {
+        type: OptionType.STRING,
+        description: "Set the audio bitrate (e.g., 1329600 for 1329.60 kbps)",
+        default: "1329600",
+        onChange: (value) => {
+            if (!/^\d+$/.test(value)) {
+                settings.store.bitrate = "1329600"; // reset to default if invalid
+            }
+        },
+    },
+});
 
 const hookSetTransceiverEncodingParameters = () => {
     const originalSetParameters = UnifiedConnection.prototype.setTransceiverEncodingParameters;
     UnifiedConnection.prototype.setTransceiverEncodingParameters = function (parameters) {
         if (parameters && parameters.encodings) {
             for (const encoding of parameters.encodings) {
-                encoding.maxBitrate = 1329600; // Set this to the desired bitrate (1329.60 kbps)
-                encoding.channels = 2; // Enable stereo
+                encoding.maxBitrate = parseInt(settings.store.bitrate, 10); // Set bitrate from settings
+                encoding.channels = settings.store.enableStereo ? 2 : 1; // Toggle stereo or mono
             }
         }
         return originalSetParameters.apply(this, arguments);
@@ -26,7 +46,7 @@ const hookSetTransportOptions = () => {
     RTCRtpSender.prototype.setTransportOptions = function (options) {
         if (options && options.audio && options.audio.transportOptions) {
             options.audio.transportOptions.encodingParams = {
-                channels: 2 // Enable stereo
+                channels: settings.store.enableStereo ? 2 : 1 // Toggle stereo or mono
             };
         }
         return originalSetTransportOptions.apply(this, arguments);
@@ -43,5 +63,6 @@ window.addEventListener('load', init);
 export default definePlugin({
     name: "HQ Stereo Mic",
     description: "This plugin allows stereo mic and enforcing a higher bitrate.",
-    authors: [Devs.Chaython],
+    authors: [{ name: "Chaython", id: 799911395797893120n }],
+settings,
 });
