d0526c2e4a62aaf68f301711be9670429bf5750f
StellarDev <ExooDev@proton.me>
2025-01-11 02:40:12 -0600

    Update index.ts

--- src/plugins/moreCommands/index.ts
+++ src/plugins/moreCommands/index.ts
@@ -19,7 +19,7 @@
 import { ApplicationCommandInputType, findOption, OptionalMessageOption, RequiredMessageOption, sendBotMessage } from "@api/Commands";
 import { Devs } from "@utils/constants";
 import definePlugin from "@utils/types";
-
+import axios from "axios";
 
 function mock(input: string): string {
     let output = "";
@@ -31,7 +31,7 @@ function mock(input: string): string {
 
 export default definePlugin({
     name: "MoreCommands",
-    description: "echo, lenny, mock",
+    description: "Echo, Lenny, Mock, and More",
     authors: [Devs.Arjix, Devs.echo, Devs.Samu],
     commands: [
         {
@@ -41,7 +41,6 @@ export default definePlugin({
             inputType: ApplicationCommandInputType.BOT,
             execute: (opts, ctx) => {
                 const content = findOption(opts, "message", "");
-
                 sendBotMessage(ctx.channel.id, { content });
             },
         },
@@ -61,5 +60,137 @@ export default definePlugin({
                 content: mock(findOption(opts, "message", ""))
             }),
         },
+        {
+            name: "reverse",
+            description: "Reverses the input message",
+            options: [RequiredMessageOption],
+            execute: opts => ({
+                content: findOption(opts, "message", "").split("").reverse().join("")
+            }),
+        },
+        {
+            name: "uppercase",
+            description: "Converts the message to uppercase",
+            options: [RequiredMessageOption],
+            execute: opts => ({
+                content: findOption(opts, "message", "").toUpperCase()
+            }),
+        },
+        {
+            name: "lowercase",
+            description: "Converts the message to lowercase",
+            options: [RequiredMessageOption],
+            execute: opts => ({
+                content: findOption(opts, "message", "").toLowerCase()
+            }),
+        },
+        {
+            name: "wordcount",
+            description: "Counts the number of words in the message",
+            options: [RequiredMessageOption],
+            execute: opts => {
+                const message = findOption(opts, "message", "");
+                const wordCount = message.trim().split(/\s+/).length;
+                return {
+                    content: `The message contains ${wordCount} words.`
+                };
+            },
+        },
+        {
+            name: "shrinkurl",
+            description: "Shrinks a long URL",
+            options: [RequiredMessageOption],
+            execute: async opts => {
+                const url = findOption(opts, "message", "");
+                try {
+                    const response = await axios.post("https://api.shrtco.de/v2/shorten", {
+                        url: url
+                    });
+                    return {
+                        content: `Shortened URL: ${response.data.result.full_short_link}`
+                    };
+                } catch (error) {
+                    return {
+                        content: "There was an error shortening the URL."
+                    };
+                }
+            },
+        },
+        {
+            name: "joke",
+            description: "Tells a random joke",
+            options: [],
+            execute: async () => {
+                try {
+                    const response = await axios.get("https://official-joke-api.appspot.com/jokes/random");
+                    const joke = response.data[0];
+                    return {
+                        content: `${joke.setup} - ${joke.punchline}`
+                    };
+                } catch (error) {
+                    return {
+                        content: "Sorry, I couldn't fetch a joke right now."
+                    };
+                }
+            },
+        },
+        {
+            name: "servertime",
+            description: "Displays the current server time",
+            options: [],
+            execute: () => {
+                const currentTime = new Date().toLocaleString();
+                return {
+                    content: `The current server time is: ${currentTime}`
+                };
+            },
+        },
+        {
+            name: "ping",
+            description: "Pings the bot to check if it's responding",
+            options: [],
+            execute: () => ({
+                content: "Pong!"
+            }),
+        },
+        {
+            name: "rolldice",
+            description: "Roll a die with the specified number of sides",
+            options: [RequiredMessageOption],
+            execute: opts => {
+                const sides = parseInt(findOption(opts, "message", "6"));
+                const roll = Math.floor(Math.random() * sides) + 1;
+                return {
+                    content: `You rolled a ${roll}!`
+                };
+            },
+        },
+        {
+            name: "flipcoin",
+            description: "Flips a coin and returns heads or tails",
+            options: [],
+            execute: () => {
+                const flip = Math.random() < 0.5 ? "Heads" : "Tails";
+                return {
+                    content: `The coin landed on: ${flip}`
+                };
+            },
+        },
+        {
+            name: "ask",
+            description: "Ask a yes/no question and get an answer",
+            options: [RequiredMessageOption],
+            execute: opts => {
+                const question = findOption(opts, "message", "");
+                const responses = [
+                    "Yes", "No", "Maybe", "Ask again later", "Definitely not", "It is certain"
+                ];
+                const response = responses[Math.floor(Math.random() * responses.length)];
+                return {
+                    content: `${question} - ${response}`
+                };
+            },
+        },
     ]
 });
+
