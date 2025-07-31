d484911c72cf11ecddbc1fe48d6fbf010d551417
Johannes7k75 <Jonashebenstreit1@gmail.com>
2025-07-31 20:38:18 +0200

    Remove native.ts (not needed)

--- src/equicordplugins/youtubeMusicControls/native.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Vencord, a Discord client mod
- * Copyright (c) 2025 Vendicated and contributors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-
-import { CspPolicies, ImageSrc } from "@main/csp";
-
-CspPolicies["*.googleusercontent.com"] = ImageSrc; // Google User Content (YouTube, etc.)
