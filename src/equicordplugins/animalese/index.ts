d1ed754f1a51c4b3d539cc6aacea637f91ab392a
mocha <mocha@mochas-MacBook-Pro.local>
2025-08-23 00:00:29 -0400

    import devs :3

--- src/equicordplugins/animalese/index.ts
+++ src/equicordplugins/animalese/index.ts
@@ -5,6 +5,7 @@
  */
 
 import { definePluginSettings } from "@api/Settings";
+import { EquicordDevs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
 import { FluxDispatcher } from "@webpack/common";
 
