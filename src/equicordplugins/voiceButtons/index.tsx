f9aa57f0d88e5c8c22c5cc90d924b25b766ab3bb
Eazvy <57739965+Eazvy@users.noreply.github.com>
2025-09-11 22:00:35 -0400

    Add VoiceButtons
    Allows you to easily deafen / mute people via voice-channel. With customizable settings that allows you to choose whether you want to toggle their video or soundboard in settings.
    Source was taken from ClickToChat in vencord just added a few more things to make i way better

---
+++ src/equicordplugins/voiceButtons/index.tsx
@@ -0,0 +1,71 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import definePlugin, {OptionType} from "@utils/types";
+import { definePluginSettings, Settings } from "@api/Settings";
+import { UserChatButton, UserMuteButton, UserDeafenButton } from "./components/UserChatButtons";
+import { User } from "@vencord/discord-types";
+import { React } from "@webpack/common";
+import { EquicordDevs } from "@utils/constants";
+
+export const settings = definePluginSettings({
+    showChatButton: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Whether you want to display the chat button",
+        restartNeeded: true,
+    },
+    showMuteButton: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Whether you want to display the mute button",
+        restartNeeded: true,
+    },
+    showDeafenButton: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Whether you want to display the deafen button",
+        restartNeeded: true,
+    },
+    muteSoundboard: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Toggles their soundboard upon clicking deafen button.",
+        restartNeeded: false,
+    },
+    disableVideo: {
+        type: OptionType.BOOLEAN,
+        default: true,
+        description: "Toggles their video upon clicking deafen button.",
+        restartNeeded: false,
+    },
+});
+
+export default definePlugin({
+    name: "VoiceButtons",
+    description: "Quickly DM, mute, or deafen any user right from the voice-call panel.",
+    authors: [{name: "nicola02nb",id: 257900031351193600n}, EquicordDevs.omaw],
+    settings,
+    patches: [
+        {
+            find: "\"avatarContainerClass\",\"userNameClassName\"",
+            replacement: {
+                match: /(\((\i),\i\){?=.{0,850}\.flipped])(:\i}\),children:\[)/,
+                replace: "$1$3$self.renderPing($2?.user),"
+            }
+        }
+    ],
+    renderPing(user?: User) {
+        if (!user) return null;
+        return (
+            <div style={{ display: "flex", gap: "4px" }}>
+                {settings.store.showChatButton && <UserChatButton user={user} />}
+                {settings.store.showMuteButton && <UserMuteButton user={user} />}
+                {settings.store.showDeafenButton && <UserDeafenButton user={user} />}
+            </div>
+        );
+    }
+});
