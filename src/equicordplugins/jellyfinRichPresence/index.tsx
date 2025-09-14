27c0c119be1dfef48b3f04b2bca16d33908ccdb4
SerStars <90714930+SerStars@users.noreply.github.com>
2025-09-14 18:19:00 +0200

    Update instructions (Equicord#390)

--- src/equicordplugins/jellyfinRichPresence/index.tsx
+++ src/equicordplugins/jellyfinRichPresence/index.tsx
@@ -133,12 +133,20 @@ export default definePlugin({
             <Forms.FormTitle tag="h3">How to get an API key</Forms.FormTitle>
             <Forms.FormText>
                 Auth token can be found by following these steps:
-                Log into your Jellyfin web interface
-                Open your browser's Developer Tools (usually F12 or right-click then Inspect)
-                Go to the "Network" tab in Developer Tools
-                Look for requests to your Jellyfin server
-                In the request headers, find "X-MediaBrowser-Token" or "Authorization"
-
+                <ol style={{ marginTop: 8, marginBottom: 8, paddingLeft: 20 }}>
+                    <li>1. Log into your Jellyfin instance</li>
+                    <li>2. Open your browser's Developer Tools (usually F12 or right-click then Inspect)</li>
+                    <li>3. Go to the <b>Network</b> tab in Developer Tools</li>
+                    <li>4. Look for requests to your Jellyfin server</li>
+                    <li>
+                        5. In the request headers, find <code>X-MediaBrowser-Token</code> or <code>Authorization</code>
+                        <br />
+                        <i>
+                            Easiest way: press <b>Ctrl+F</b> in the Developer Tools and search for <code>X-MediaBrowser-Token</code>
+                        </i>
+                    </li>
+                </ol>
+                <br />
                 You'll also need your User ID, which can be found in the url of your user profile page.
             </Forms.FormText>
         </>
