c05c68bcc70610fc36d27485e6bbecdffd386705
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2025-09-25 21:33:35 -0400

    bleh

--- src/equicordplugins/equicordHelper/index.tsx
+++ src/equicordplugins/equicordHelper/index.tsx
@@ -6,13 +6,21 @@
 
 import "@equicordplugins/_misc/styles.css";
 
-import { definePluginSettings } from "@api/Settings";
-import { Devs, EquicordDevs } from "@utils/constants";
+import { definePluginSettings, useSettings } from "@api/Settings";
+import ErrorBoundary from "@components/ErrorBoundary";
+import { WarningIcon } from "@components/Icons";
+import { AddonCard } from "@components/settings";
+import { ExcludedReasons, PluginDependencyList } from "@components/settings/tabs/plugins";
+import { PluginCard } from "@components/settings/tabs/plugins/PluginCard";
+import { Devs, EQUIBOT_USER_ID, EquicordDevs } from "@utils/constants";
 import { Logger } from "@utils/Logger";
 import { isEquicordGuild, isEquicordSupport } from "@utils/misc";
 import definePlugin, { OptionType } from "@utils/types";
-import { Button, Flex, showToast, Toasts } from "@webpack/common";
+import { Message } from "@vencord/discord-types";
+import { Button, Flex, showToast, Toasts, Tooltip, TooltipContainer } from "@webpack/common";
 import { JSX } from "react";
+
+import plugins, { ExcludedPlugins } from "~plugins";
 
 import { toggleEnabled } from "./utils";
 
@@ -70,11 +78,11 @@ export default definePlugin({
         },
     ],
     renderMessageAccessory(props) {
-        return pluginToggleButtons(props);
+        return pluginButtons(props);
     }
 });
 
-function pluginToggleButtons(props) {
+function pluginButtons(props) {
     const buttons = [] as JSX.Element[];
     const msg = props.message.content?.toLowerCase() ?? "";
 
@@ -122,7 +130,144 @@ function pluginToggleButtons(props) {
         );
     }
 
-    return buttons.length
-        ? <Flex>{buttons}</Flex>
-        : null;
+    return (
+        <>
+            {buttons.length > 0 && <Flex>{buttons}</Flex>}
+            <PluginCards message={props.message} />
+        </>
+    );
 }
+
+
+function ChatPluginCard({ url, description }: { url: string, description: string; }) {
+    const pluginNameFromUrl = new URL(url).pathname.split("/")[2];
+
+    const actualPluginName = Object.keys(plugins).find(name =>
+        name.toLowerCase() === pluginNameFromUrl?.toLowerCase()
+    );
+
+    const pluginName = actualPluginName || pluginNameFromUrl;
+
+    useSettings([`plugins.${pluginName ?? ""}.enabled`]);
+
+    if (!pluginName) return null;
+
+    const p = plugins[pluginName];
+    const excludedPlugin = ExcludedPlugins[pluginName];
+
+    if (excludedPlugin || !p) {
+        const toolTipText = !p
+            ? `${pluginName} is only available on the ${ExcludedReasons[ExcludedPlugins[pluginName]]}`
+            : "This plugin is not on this version of Equicord. Try updating!";
+
+        const card = (
+            <AddonCard
+                name={pluginName}
+                description={description || toolTipText}
+                enabled={false}
+                setEnabled={() => { }}
+                disabled={true}
+                infoButton={<WarningIcon />}
+            />
+        );
+
+        return description
+            ? <TooltipContainer text={toolTipText}>{card}</TooltipContainer>
+            : card;
+    }
+
+    const onRestartNeeded = () => showToast("A restart is required for the change to take effect!");
+
+    const required = Vencord.Plugins.isPluginRequired(pluginName);
+    const dependents = Vencord.Plugins.calculatePluginDependencyMap()[p.name]?.filter(d => Vencord.Plugins.isPluginEnabled(d));
+
+    if (required) {
+        const tooltipText = p.required || !dependents.length
+            ? "This plugin is required for Equicord to function."
+            : <PluginDependencyList deps={dependents} />;
+
+        return (
+            <Tooltip text={tooltipText} key={p.name}>
+                {({ onMouseLeave, onMouseEnter }) =>
+                    <PluginCard
+                        key={p.name}
+                        onMouseLeave={onMouseLeave}
+                        onMouseEnter={onMouseEnter}
+                        onRestartNeeded={onRestartNeeded}
+                        plugin={p}
+                        disabled
+                    />
+                }
+            </Tooltip>
+        );
+    }
+
+    return (
+        <PluginCard
+            key={p.name}
+            onRestartNeeded={onRestartNeeded}
+            plugin={p}
+        />
+    );
+}
+
+const PluginCards = ErrorBoundary.wrap(function PluginCards({ message }: { message: Message; }) {
+    const seenPlugins = new Set<string>();
+    const pluginCards: JSX.Element[] = [];
+
+    // Process embeds
+    message.embeds?.forEach(embed => {
+        if (!embed.url?.startsWith("https://equicord.org/plugins/") && !embed.url?.startsWith("https://vencord.dev/plugins/")) return;
+
+        const pluginNameFromUrl = new URL(embed.url).pathname.split("/")[2];
+        const actualPluginName = Object.keys(plugins).find(name =>
+            name.toLowerCase() === pluginNameFromUrl?.toLowerCase()
+        );
+        const pluginName = actualPluginName || pluginNameFromUrl;
+
+        if (!pluginName || seenPlugins.has(pluginName)) return;
+        seenPlugins.add(pluginName);
+
+        pluginCards.push(
+            <ChatPluginCard
+                key={embed.url}
+                url={embed.url}
+                description={embed.rawDescription}
+            />
+        );
+    });
+
+    // Process components
+    const components = (message.components?.[0] as any)?.components;
+    const isEquicord = isEquicordGuild(message.channel_id) && isEquicordSupport(message.author.id) || message.author.id === EQUIBOT_USER_ID;
+    if (isEquicord && components?.length >= 5) {
+        const description = components[1]?.content;
+        const pluginUrl = components.find((c: any) => c?.components)?.components[0]?.url;
+        if (pluginUrl?.startsWith("https://equicord.org/plugins/") || pluginUrl?.startsWith("https://vencord.dev/plugins/")) {
+            const pluginNameFromUrl = new URL(pluginUrl).pathname.split("/")[2];
+            const actualPluginName = Object.keys(plugins).find(name =>
+                name.toLowerCase() === pluginNameFromUrl?.toLowerCase()
+            );
+            const pluginName = actualPluginName || pluginNameFromUrl;
+
+            if (pluginName && !seenPlugins.has(pluginName)) {
+                seenPlugins.add(pluginName);
+                pluginCards.push(
+                    <ChatPluginCard
+                        key={pluginUrl}
+                        url={pluginUrl}
+                        description={description}
+                    />
+                );
+            }
+        }
+    }
+
+    if (pluginCards.length === 0) return null;
+
+    return (
+        <div className="vc-plugins-grid" style={{ marginTop: "0px" }}>
+            {pluginCards}
+        </div>
+    );
+}, { noop: true });
