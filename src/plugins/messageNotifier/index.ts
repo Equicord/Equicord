3abc9f3a79ad6c20056d8f0c27d3bf3c12181dde
thororen <78185467+thororen1234@users.noreply.github.com>
2025-11-23 20:48:42 -0500

    Refactor message notifier to improve user ID handling

--- src/plugins/messageNotifier/index.ts
+++ src/plugins/messageNotifier/index.ts
@@ -11,7 +11,7 @@ import { getCurrentChannel } from "@utils/discord";
 import definePlugin, { OptionType } from "@utils/types";
 import { MessageJSON } from "@vencord/discord-types";
 import { MessageType } from "@vencord/discord-types/enums";
-import { ChannelStore, GuildStore, NavigationRouter, RelationshipStore, UserStore } from "@webpack/common";
+import { ChannelStore, GuildStore, NavigationRouter, RelationshipStore } from "@webpack/common";
 
 interface MessageCreatePayload {
     guildId: string;
@@ -22,11 +22,11 @@ interface MessageCreatePayload {
 const settings = definePluginSettings({
     users: {
         type: OptionType.STRING,
-        description: "comma separated list of user ids to get message toasts for",
+        description: "Comma separated list of user ids to get message toasts for",
         default: "",
         isValid(value: string) {
             if (value === "") return true;
-            const userIds = value.split(/, ?/);
+            const userIds = value.split(",").map(ext => ext.trim());
             for (const id of userIds)
                 if (!/\d+/.test(id)) return `${id} isn't a valid user id`;
             return true;
@@ -40,30 +40,21 @@ export default definePlugin({
     description: "Get toasts for when chosen users send a message",
     settings,
     flux: {
-        MESSAGE_CREATE(payload: MessageCreatePayload) {
-            if (payload.message.type !== MessageType.DEFAULT) return;
-            if (getCurrentChannel()?.id === payload.channelId) return;
+        MESSAGE_CREATE({ message, channelId, guildId }: MessageCreatePayload) {
+            if (message.type !== MessageType.DEFAULT || getCurrentChannel()?.id === channelId) return;
 
-            const userIds = settings.store.users.split(/, ?/);
-            if (!userIds.includes(payload.message.author.id)) return;
+            const userIds = settings.store.users.split(",").map(id => id.trim());
+            if (!userIds.includes(message.author.id)) return;
 
-            const guild = GuildStore.getGuild(payload.guildId);
-            const channel = ChannelStore.getChannel(payload.channelId);
-
-            const userDisplayName = RelationshipStore.getNickname(payload.message.author.id) ?? UserStore.getUser(payload.message.author.id).globalName!;
-            const locationName = (() => {
-                if (guild?.name !== undefined)
-                    return `${guild.name}#${channel.name}`;
-                if (channel.name === "")
-                    return "their dms";
-                return channel.name;
-            })();
-            const targetLink = `/channels/${guild?.id ?? "@me"}/${channel.id}/${payload.message.id}`;
+            const username = RelationshipStore.getNickname(message.author.id) ?? message.author.globalName ?? message.author.username;
+            const guild = GuildStore.getGuild(guildId);
+            const channel = ChannelStore.getChannel(channelId);
+            const locationName = guild ? `${guild.name}#${channel.name}` : channel?.name ?? "their dms";
 
             Notifications.showNotification({
-                title: `${userDisplayName} sent a message`,
+                title: `${username} sent a message`,
                 body: `click to jump to ${locationName}`,
-                onClick() { NavigationRouter.transitionTo(targetLink); },
+                onClick() { NavigationRouter.transitionTo(`/channels/${guild?.id ?? "@me"}/${channel.id}/${message.id}`); },
             });
         },
     },
