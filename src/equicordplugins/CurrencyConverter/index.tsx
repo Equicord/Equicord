8d3480429079dd0718e26cbfaadffe85a3d1d3ba
xMimiez <109782674+xMimiez@users.noreply.github.com>
2025-12-27 03:31:18 -0800

    Add files via upload

---
+++ src/equicordplugins/CurrencyConverter/index.tsx
@@ -0,0 +1,42 @@
+import "./style.css";
+
+import { findGroupChildrenByChildId, NavContextMenuPatchCallback } from "@api/ContextMenu";
+import definePlugin from "@utils/types";
+import { Menu } from "@webpack/common";
+
+import { settings } from "./settings";
+import { Accessory, handleConvert } from "./accessory";
+import { Icon } from "./icon";
+
+
+const messageCtxPatch: NavContextMenuPatchCallback = (children, { message }) => {
+    if (!message?.content) return;
+
+    const group = findGroupChildrenByChildId("copy-text", children);
+    if (!group) return;
+
+    const idx = group.findIndex(c => c?.props?.id === "copy-text");
+    if (idx === -1) return;
+
+    group.splice(idx + 1, 0, (
+        <Menu.MenuItem
+            id="ec-currency-convert"
+            label="Convert Currency"
+            icon={Icon}
+            action={() => handleConvert(message)}
+        />
+    ));
+};
+
+export default definePlugin({
+    name: "Currency Converter",
+    description: "Convert currencies found in messages and show the result under them.",
+    settings,
+    authors: [957164619061932045],
+
+    contextMenus: {
+        "message": messageCtxPatch
+    },
+
+    renderMessageAccessory: props => <Accessory message={props.message} />
+});
