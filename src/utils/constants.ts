b172cd02ac3bae93e934396cea2e14beaa8be6e6
chaython <chaython@live.ca>
2026-01-16 15:33:55 -0700

    some testcord plugins

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -28,6 +28,7 @@ export const EQUICORD_HELPERS = "1326406112144265257";
 export const EQUIBOP_CONTRIB_ROLE_ID = "1287079931645263968";
 export const VENCORD_CONTRIB_ROLE_ID = "1173343399470964856";
 export const EQUIBOT_USER_ID = "1243063117852835941";
+export const TESTCORD_GUILD_ID = "1173279886065029291";
 
 // Vencord
 export const VC_SUPPORT_CHANNEL_ID = "1026515880080842772";
@@ -868,6 +869,10 @@ export const EquicordDevs = Object.freeze({
         name: "bep",
         id: 0n
     },
+    Benjii: {
+        name: "Benjii",
+        id: 463702169443368970n
+    },
     llytz: {
         name: "llytz",
         id: 1271128098301022240n
@@ -1243,6 +1248,25 @@ export const EquicordDevs = Object.freeze({
     },
 } satisfies Record<string, Dev>);
 
+export const TestcordDevs = Object.freeze({
+    x2b: {
+        name: "x2b",
+        id: 996137713432530976n
+    },
+    Benjii: {
+        name: "Benjii",
+        id: 463702169443368970n
+    },
+    babystarsz: {
+        name: "babystarsz",
+        id: 1455985625441501209n
+    },
+    vampireslover: {
+        name: "vampireslover",
+        id: 1455033270709256284n
+    },
+} satisfies Record<string, Dev>);
+
 // iife so #__PURE__ works correctly
 export const VencordDevsById = /* #__PURE__*/ (() =>
     Object.freeze(Object.fromEntries(
@@ -1255,6 +1279,14 @@ export const VencordDevsById = /* #__PURE__*/ (() =>
 export const EquicordDevsById = /* #__PURE__*/ (() =>
     Object.freeze(Object.fromEntries(
         Object.entries(EquicordDevs)
+            .filter(d => d[1].id !== 0n)
+            .map(([_, v]) => [v.id, v] as const)
+    ))
+)() as Record<string, Dev>;
+
+export const TestcordDevsById = /* #__PURE__*/ (() =>
+    Object.freeze(Object.fromEntries(
+        Object.entries(TestcordDevs)
             .filter(d => d[1].id !== 0n)
             .map(([_, v]) => [v.id, v] as const)
     ))
