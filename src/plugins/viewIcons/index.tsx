6cadfce6009747ab41f0b57e612036d16919f3b0
prism <snawalt420@proton.me>
2026-01-23 16:44:36 +0100

    ViewIcons: fix patch

--- src/plugins/viewIcons/index.tsx
+++ src/plugins/viewIcons/index.tsx
@@ -194,17 +194,18 @@ export default definePlugin({
         "gdm-context": GroupDMContext
     },
 
+    getAvatarUrl(user: User) {
+        return IconUtils.getUserAvatarURL(user, true);
+    },
+
     patches: [
         // Avatar component used in User DMs "User Profile" popup in the right and User Profile Modal pfp
         {
-            find: ".overlay:void 0,status:",
-            replacement: [
-                {
-                    match: /avatarSrc:(\i),eventHandlers:(\i).+?"div",.{0,100}className:\i,/,
-                    replace: "$&style:{cursor:\"pointer\"},onClick:()=>{$self.openAvatar($1)},",
-                }
-            ],
-            all: true
+            find: "imageClassName:null!=",
+            replacement: {
+                match: /(?<=\("div",\i\(\i\(\{\},\i\),)(\{className:)/,
+                replace: '{style:{cursor:"pointer"},onClick:()=>$self.openAvatar($self.getAvatarUrl(arguments[0].user)),className:'
+            }
         },
         // Banners
         {
