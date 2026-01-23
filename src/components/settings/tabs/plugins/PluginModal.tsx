f075ca1971994f7fb4e6a7fef7a591eee4d5c4fb
prism <snawalt420@proton.me>
2026-01-23 17:31:04 +0100

    yeet pluginmodal changes

--- src/components/settings/tabs/plugins/PluginModal.tsx
+++ src/components/settings/tabs/plugins/PluginModal.tsx
@@ -31,7 +31,7 @@ import { classes, isObjectEmpty } from "@utils/misc";
 import { ModalCloseButton, ModalContent, ModalHeader, ModalProps, ModalRoot, ModalSize, openModal } from "@utils/modal";
 import { OptionType, Plugin } from "@utils/types";
 import { User } from "@vencord/discord-types";
-import { findLazy } from "@webpack";
+import { findByPropsLazy } from "@webpack";
 import { Clickable, FluxDispatcher, Forms, React, Text, Tooltip, useEffect, UserStore, UserSummaryItem, UserUtils, useState } from "@webpack/common";
 import { Constructor } from "type-fest";
 
@@ -43,8 +43,7 @@ import { GithubButton, WebsiteButton } from "./LinkIconButton";
 
 const cl = classNameFactory("vc-plugin-modal-");
 
-const AvatarStyles: Record<string, string> = findLazy(m => Object.values(m).some(v => typeof v === "string" && v.includes("moreUsers__")));
-const getAvatarStyle = (name: string) => Object.values(AvatarStyles).find(v => v.startsWith(name + "__")) ?? "";
+const AvatarStyles = findByPropsLazy("moreUsers", "emptyUser", "avatarContainer", "clickableAvatar");
 const UserRecord: Constructor<Partial<User>> = proxyLazy(() => UserStore.getCurrentUser().constructor) as any;
 
 interface PluginModalProps extends ModalProps {
@@ -138,7 +137,7 @@ export default function PluginModal({ plugin, onRestartNeeded, onClose, transiti
             <Tooltip text={plugin.authors.slice(sliceStart, sliceEnd).map(u => u.name).join(", ")}>
                 {({ onMouseEnter, onMouseLeave }) => (
                     <div
-                        className={getAvatarStyle("moreUsers")}
+                        className={AvatarStyles.moreUsers}
                         onMouseEnter={onMouseEnter}
                         onMouseLeave={onMouseLeave}
                     >
@@ -187,11 +186,11 @@ export default function PluginModal({ plugin, onRestartNeeded, onClose, transiti
                                 renderMoreUsers={renderMoreUsers}
                                 renderUser={(user: User) => (
                                     <Clickable
-                                        className={getAvatarStyle("clickableAvatar")}
+                                        className={AvatarStyles.clickableAvatar}
                                         onClick={() => openContributorModal(user)}
                                     >
                                         <img
-                                            className={getAvatarStyle("avatar")}
+                                            className={AvatarStyles.avatar}
                                             src={user.getAvatarURL(void 0, 80, true)}
                                             alt={user.username}
                                             title={user.username}
