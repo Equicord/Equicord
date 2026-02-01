f1255ad673ec8bdf434b81d8c84d22a7f2c7857f
Eazvy <57739965+Eazvy@users.noreply.github.com>
2026-02-01 12:47:49 -0500

    prism fix

--- src/equicordplugins/AutoJump/index.tsx
+++ src/equicordplugins/AutoJump/index.tsx
@@ -1,70 +1,19 @@
-/*
- * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-
-import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import { EquicordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { Menu, ChannelStore, ComponentDispatch } from "@webpack/common";
+import { ComponentDispatch } from "@webpack/common";
 
-interface ChannelSelectEvent {
-    type: "CHANNEL_SELECT";
-    channelId: string | null;
-    guildId: string | null;
-    messageId?: string | null;
-}
+let lastChannelId: string | null = null;
 
-let lastChannelId = "0";
-
-function autoJump(channel: any) {
-    const guildId = channel.guild_id ?? "@me";
-    const channelId = channel.id;
-
-    if (channelId === lastChannelId) return;
-
-    lastChannelId = channelId;
-
-    ComponentDispatch.dispatch("SCROLLTO_PRESENT");
-}
-
-const MenuPatch: NavContextMenuPatchCallback = (children) => {
-    children.push(
-        <Menu.MenuItem
-            id="auto-jump"
-            label="Jump to Last Message"
-            action={() => ComponentDispatch.dispatch("SCROLLTO_PRESENT")}
-        />
-    );
-};
-
+// credits to prism for removing useless pieces of code 
 export default definePlugin({
     name: "AutoJump",
-    description: "Automatically jumps to the last message when selecting a channel.",
+    description: "Automatically jump to the bottom when switching channels.",
     authors: [EquicordDevs.omaw],
-    
-    contextMenus: {
-        "channel-context": MenuPatch,
-        "user-context": MenuPatch,
-        "thread-context": MenuPatch
-    },
-    
     flux: {
-        async CHANNEL_SELECT(event: ChannelSelectEvent) {
-            const { guildId, channelId, messageId } = event;
-            
-            if (!guildId || !channelId) return;
-            if (lastChannelId === channelId) return;
-            if (messageId) {
-                lastChannelId = channelId;
-                return;
-            }
-            
-            const channel = ChannelStore.getChannel(channelId);
-            if (!channel) return;
-            
-            autoJump(channel);
+        CHANNEL_SELECT({ channelId, messageId }) {
+            if (!channelId || messageId || lastChannelId === channelId) return;
+            lastChannelId = channelId;
+            ComponentDispatch.dispatch("SCROLLTO_PRESENT");
         }
     }
 });
