58da34bb42941fa195ad87f892b070cd7fdae108
Eazvy <57739965+Eazvy@users.noreply.github.com>
2026-02-01 14:45:01 -0500

    add menus cause why not?

--- src/equicordplugins/AutoJump/index.tsx
+++ src/equicordplugins/AutoJump/index.tsx
@@ -6,14 +6,29 @@
 
 import { EquicordDevs } from "@utils/constants";
 import definePlugin from "@utils/types";
-import { ComponentDispatch } from "@webpack/common";
+import { Menu, ComponentDispatch } from "@webpack/common";
+import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 
 let lastChannelId: string | null = null;
+const MenuPatch: NavContextMenuPatchCallback = (children) => {
+    children.push(
+        <Menu.MenuItem
+            id="auto-jump"
+            label="Jump to Last Message"
+            action={() => ComponentDispatch.dispatch("SCROLLTO_PRESENT")}
+        />
+    );
+};
 
 export default definePlugin({
     name: "AutoJump",
     description: "Automatically jump to the bottom when switching channels.",
     authors: [EquicordDevs.omaw],
+    contextMenus: {
+        "channel-context": MenuPatch,
+        "user-context": MenuPatch,
+        "thread-context": MenuPatch
+    },
     flux: {
         CHANNEL_SELECT({ channelId, messageId }) {
             if (!channelId || messageId || lastChannelId === channelId) return;
