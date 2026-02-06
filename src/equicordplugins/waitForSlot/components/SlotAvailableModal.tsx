194e49de28cfe210bbf809dca1f84f6e8b91031b
Eazvy <57739965+Eazvy@users.noreply.github.com>
2026-02-06 09:00:36 -0500

    update

--- src/equicordplugins/waitForSlot/components/SlotAvailableModal.tsx
+++ src/equicordplugins/waitForSlot/components/SlotAvailableModal.tsx
@@ -3,9 +3,12 @@ import { Button } from "@components/Button";
 import { Paragraph } from "@components/Paragraph";
 import { ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalProps, ModalRoot, ModalSize } from "@utils/modal";
 import type { Channel } from "@vencord/discord-types";
-import { React } from "@webpack/common";
+import { GuildStore, IconUtils, React } from "@webpack/common";
 
 export function SlotAvailableModal({ modalProps, channel, onJoin, }: { modalProps: ModalProps; channel: Channel; onJoin: () => void; }) {
+    const guild = GuildStore.getGuild(channel.guild_id);
+    const guildIcon = guild?.icon == null ? undefined : IconUtils.getGuildIconURL({ id: guild.id, icon: guild.icon, size: 32 });
+
     return (
         <ModalRoot {...modalProps} size={ModalSize.SMALL}>
             <ModalHeader className="vc-wfs-header">
@@ -13,8 +16,12 @@ export function SlotAvailableModal({ modalProps, channel, onJoin, }: { modalProp
                 <ModalCloseButton onClick={modalProps.onClose} />
             </ModalHeader>
             <ModalContent>
+                <div className="vc-wfs-vc-row">
+                    {guildIcon && <img className="vc-wfs-guild-icon" src={guildIcon} alt="" />}
+                    <BaseText size="md" weight="semibold">{channel.name}</BaseText>
+                </div>
                 <Paragraph size="md" className="vc-wfs-available-text">
-                    Would you like to join <strong>{channel.name}</strong>?
+                    A slot is available for this voice channel. Would you like to join?
                 </Paragraph>
             </ModalContent>
             <ModalFooter justify="start" direction="horizontal" className="vc-wfs-footer">
