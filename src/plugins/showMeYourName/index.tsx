0e4256dbbb5f4574ac081b97a5935cec4c6fc667
Etorix <92535668+EtorixDev@users.noreply.github.com>
2026-02-19 16:03:35 -0800

    Fix color.

--- src/plugins/showMeYourName/index.tsx
+++ src/plugins/showMeYourName/index.tsx
@@ -136,7 +136,8 @@ function resolveColor(
         secondaryAdjusted = secondaryColor && percentage ? adjustBrightness(secondaryColor, parseInt(percentage)) : secondaryColor;
         tertiaryAdjusted = tertiaryColor && percentage ? adjustBrightness(tertiaryColor, parseInt(percentage)) : tertiaryColor;
     } else {
-        primaryColor = savedColor;
+        primaryColor = toCSS(savedColor);
+        primaryAdjusted = primaryColor;
     }
 
     gradient = !canUseGradient || !secondaryColor || forceDefault
