d65a238b1af4536e0ae20db6e2cf1c60b9871232
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-02-26 10:39:29 -0500

    Update settings.ts

--- src/equicordplugins/voiceButtons/settings.ts
+++ src/equicordplugins/voiceButtons/settings.ts
@@ -30,25 +30,21 @@ export const settings = definePluginSettings({
         type: OptionType.BOOLEAN,
         default: true,
         description: "Toggles their soundboard upon clicking deafen button.",
-        restartNeeded: false,
     },
     disableVideo: {
         type: OptionType.BOOLEAN,
         default: true,
         description: "Toggles their video upon clicking deafen button.",
-        restartNeeded: false,
     },
     useServer: {
         type: OptionType.BOOLEAN,
         description: "Use server mute/deafen instead of local when you have permission.",
         default: false,
-        restartNeeded: false,
     },
     serverSelf: {
         type: OptionType.BOOLEAN,
         description: "Deafen / Mute yourself on the server when using mute/deafen.",
         default: false,
-        restartNeeded: false,
     },
     showButtonsSelf: {
         type: OptionType.SELECT,
