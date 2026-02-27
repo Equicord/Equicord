04baae63fa68a21ab653df03c0b47daa84feddcf
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-02-27 18:47:56 -0500

    Update index.ts

--- src/equicordplugins/hidePersonalization/index.ts
+++ src/equicordplugins/hidePersonalization/index.ts
@@ -9,75 +9,72 @@ import { EquicordDevs } from "@utils/index";
 import definePlugin, { OptionType } from "@utils/types";
 
 export const settings = definePluginSettings({
-  avatarDecoration: {
-    type: OptionType.BOOLEAN,
-    default: true,
-    description: "Hide avatar decorations.",
-    restartNeeded: true,
-  },
-  nameplate: {
-    type: OptionType.BOOLEAN,
-    default: true,
-    description: "Hide nameplates.",
-    restartNeeded: true,
-  },
-  profileEffect: {
-    type: OptionType.BOOLEAN,
-    default: true,
-    description: "Hide profile animation effects on open.",
-    restartNeeded: true,
-  },
-  clanTag: {
-    type: OptionType.BOOLEAN,
-    default: true,
-    description: "Hide clan tags.",
-    restartNeeded: true,
-  },
+    avatarDecoration: {
+        type: OptionType.BOOLEAN,
+        description: "Hide avatar decorations.",
+        default: true,
+        restartNeeded: true,
+    },
+    nameplate: {
+        type: OptionType.BOOLEAN,
+        description: "Hide nameplates.",
+        default: true,
+        restartNeeded: true,
+    },
+    profileEffect: {
+        type: OptionType.BOOLEAN,
+        description: "Hide profile animation effects on open.",
+        default: true,
+        restartNeeded: true,
+    },
+    clanTag: {
+        type: OptionType.BOOLEAN,
+        description: "Hide clan tags.",
+        default: true,
+        restartNeeded: true,
+    },
 });
 
 export default definePlugin({
-  name: "HidePersonalization",
-  description: "Hides personalization features.",
-  authors: [EquicordDevs.Leon135],
-  settings,
-
-  patches: [
-    {
-      // Avatar decoration
-      find: "getAvatarDecorationURL:w",
-      replacement: {
-        match: /function w\(e\)\{let\{avatarDecoration:(\w+),/,
-        replace: "function w(e){return null;let{avatarDecoration:$1,"
-      },
-      predicate: () => settings.store.avatarDecoration,
-    },
-    {
-      // Nameplate
-      find: "WK:()=>l",
-      replacement: {
-        match: /function l\(e\)\{return null==e\?null:\{skuId/,
-        replace: "function l(e){return null;return{skuId"
-      },
-      predicate: () => settings.store.nameplate,
-    },
-    {
-      // Profile banner animation effect
-      find: "profileEffectConfig:_,layerData:S",
-      replacement: {
-        match: /,T=e=>\{let t=\(0,\w+\.\w+\)\(\)/,
-        replace: ",T=e=>{return null;let t=(0,d.j)()"
-      },
-      predicate: () => settings.store.profileEffect,
-    },
-    {
-      // Clan tag
-      find: "Wb:()=>h",
-      replacement: {
-        match: /function h\(e,t,n\)\{/,
-        replace: "function h(e,t,n){return false;"
-      }
-    }
-
-  ]
-
+    name: "HidePersonalization",
+    description: "Hides personalization features.",
+    authors: [EquicordDevs.Leon135],
+    settings,
+    patches: [
+        {
+            // Avatar decoration
+            find: "getAvatarDecorationURL:",
+            replacement: {
+                match: /function \i\(\i\)\{(?=.{0,10}avatarDecoration)/,
+                replace: "$&return null;"
+            },
+            predicate: () => settings.store.avatarDecoration,
+        },
+        {
+            // Nameplate
+            find: "#{intl::AVATAR_MALLOW}",
+            replacement: {
+                match: /function \i\(\i\)\{(?=.{0,25}skuId:)/,
+                replace: "$&return null;"
+            },
+            predicate: () => settings.store.nameplate,
+        },
+        {
+            // Profile banner animation effect
+            find: "bannerAdjustment,noBorderRadius",
+            replacement: {
+                match: /\i=\i=>\{(?=.{0,50}\.useReducedMotion\))/,
+                replace: "$&return null;"
+            },
+            predicate: () => settings.store.profileEffect,
+        },
+        {
+            // Clan tag
+            find: ".GuildFeatures.GUILD_TAGS)",
+            replacement: {
+                match: /(?<=\.profile\?\.badge.{0,50}\i\)\{)/,
+                replace: "return false;"
+            }
+        }
+    ]
 });
