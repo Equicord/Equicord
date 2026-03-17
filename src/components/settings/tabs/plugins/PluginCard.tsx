9f03f4d5f3a71af18f045468ca0c807eae59856c
playfairs <root@playfairs.cc>
2026-03-16 19:14:14 -0500

    FavoritePlugins: Change icon to Star

--- src/components/settings/tabs/plugins/PluginCard.tsx
+++ src/components/settings/tabs/plugins/PluginCard.tsx
@@ -22,7 +22,7 @@ import { openPluginModal } from "./PluginModal";
 const logger = new Logger("PluginCard");
 const cl = classNameFactory("vc-plugins-");
 
-function HeartIcon({ filled, className }: { filled?: boolean; className?: string; }) {
+function StarIcon({ filled, className }: { filled?: boolean; className?: string; }) {
     return (
         <svg
             className={classes(className, "vc-icon")}
@@ -34,7 +34,7 @@ function HeartIcon({ filled, className }: { filled?: boolean; className?: string
             stroke="currentColor"
             strokeWidth="2"
         >
-            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
         </svg>
     );
 }
@@ -167,9 +167,9 @@ export function PluginCard({ plugin, disabled, onRestartNeeded, onMouseEnter, on
                             role="switch"
                             onClick={() => onToggleFavorite(plugin.name)}
                             className={cl("info-button")}
-                            style={{ color: isFavorite ? "#f04747" : "currentColor" }}
+                            style={{ color: isFavorite ? "#e6b800" : "currentColor" }}
                         >
-                            <HeartIcon className={cl("info-icon")} filled={isFavorite} />
+                            <StarIcon className={cl("info-icon")} filled={isFavorite} />
                         </button>
                     )}
                     <button
