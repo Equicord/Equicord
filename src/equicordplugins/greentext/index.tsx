cb7b5202e5bc0e48f0eb25c4e32ac99fc2fd5f61
Nonsensical <126889064+NonsensicalOne@users.noreply.github.com>
2026-03-20 00:13:42 +0300

    feat: Greentext plugin

---
+++ src/equicordplugins/greentext/index.tsx
@@ -0,0 +1,45 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { Parser } from "@webpack/common";
+
+function makeColorRule(char: string, regex: RegExp, color: string) {
+    return {
+        order: 9,
+        match: (source: string, state: any) => {
+            if (state.prevCapture?.[0].slice(-1) !== "\n" && state.prevCapture != null)
+                return null;
+            return regex.exec(source);
+        },
+        parse: (capture: string[]) => ({ content: capture[0] }),
+        react: (node: any) => <span style={{ color }}>{node.content}</span>,
+        requiredFirstCharacters: [char],
+    } as any;
+}
+
+export default definePlugin({
+    name: "Greentext",
+    description: "Renders imageboard-style colored text (>, <, ^).",
+    authors: [Devs.NonsensicalOne],
+
+    start() {
+        Parser.defaultRules.greentext = makeColorRule(">", /^>[^\n]*/, "#789922");
+        Parser.defaultRules.orangetext = makeColorRule("<", /^<(?!@)[^\n]*/, "#f6750b");
+        Parser.defaultRules.bluetext = makeColorRule("^", /^\^[^\n]*/, "#6577E6");
+
+        this._oldParse = Parser.parse;
+        Parser.parse = Parser.reactParserFor(Parser.defaultRules);
+    },
+
+    stop() {
+        delete Parser.defaultRules.greentext;
+        delete Parser.defaultRules.orangetext;
+        delete Parser.defaultRules.bluetext;
+        Parser.parse = this._oldParse;
+    }
+});
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -143,6 +143,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "Nickyux",
         id: 427146305651998721n
     },
+    NonsensicalOne: {
+        name: "nonsensical1",
+        id: 917403205250457641n
+    },
     mantikafasi: {
         name: "mantikafasi",
         id: 287555395151593473n
