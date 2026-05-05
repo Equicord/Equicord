2f20981a36e90bcd72261950863306a2ab7abe0e
Etorix <92535668+EtorixDev@users.noreply.github.com>
2026-05-05 02:50:33 -0700

    ErrorBoundary.

--- src/equicordplugins/questify/index.tsx
+++ src/equicordplugins/questify/index.tsx
@@ -15,7 +15,7 @@ import definePlugin, { StartAt } from "@utils/types";
 import type { Quest, QuestUserStatus } from "@vencord/discord-types";
 import { findComponentByCodeLazy, onceReady } from "@webpack";
 import { QuestStore } from "@webpack/common";
-import { JSX } from "react";
+import type { JSX } from "react";
 
 import { disguiseHomeButton, QuestButton, showQuestButton } from "./components/questButton";
 import { QuestTileContextMenu } from "./components/questTileContextMenu";
@@ -86,13 +86,15 @@ const Button = findComponentByCodeLazy("BUTTON_LOADING_STARTED_LABEL)),");
 
 function enrolledIncompleteButton(questifyButtonProps: QuestButtonPatchProps, size: string): JSX.Element {
     return (
-        <Button
-            size={size}
-            variant="secondary"
-            disabled={false}
-            fullWidth={false}
-            {...questifyButtonProps}
-        />
+        <ErrorBoundary noop>
+            <Button
+                size={size}
+                variant="secondary"
+                disabled={false}
+                fullWidth={true}
+                {...questifyButtonProps}
+            />
+        </ErrorBoundary>
     );
 }
 
