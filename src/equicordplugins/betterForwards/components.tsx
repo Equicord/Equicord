36425a67ca63a274be2b0b049eebb06291f90705
Davri <42148912+Davr1@users.noreply.github.com>
2026-06-16 20:52:16 +0200

    BetterForwards: overhaul

---
+++ src/equicordplugins/betterForwards/components.tsx
@@ -0,0 +1,292 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { BaseText } from "@components/BaseText";
+import { Flex, FlexProps } from "@components/Flex";
+import { RightArrow } from "@components/Icons";
+import { Margins } from "@components/margins";
+import { iconsModule } from "@equicordplugins/_core/concatenatedModules";
+import { Message, MessageAttachment } from "@vencord/discord-types";
+import { ChannelType } from "@vencord/discord-types/enums";
+import { findByCodeLazy, findComponentByCodeLazy, findCssClassesLazy } from "@webpack";
+import {
+    ChannelStore,
+    DateUtils,
+    GuildStore,
+    IconUtils,
+    match,
+    NavigationRouter,
+    Popout,
+    SelectedGuildStore,
+    SnowflakeUtils,
+    Tooltip,
+    useMemo,
+    useRef,
+    UserStore,
+    useStateFromStores
+} from "@webpack/common";
+import { Dispatch, PropsWithChildren, SetStateAction } from "react";
+
+import { cl, ForwardOptions } from ".";
+
+type AttachmentType =
+    | "IMAGE"
+    | "VIDEO"
+    | "CLIP"
+    | "AUDIO"
+    | "VISUAL_PLACEHOLDER"
+    | "PLAINTEXT_PREVIEW"
+    | "OTHER"
+    | "INVALID";
+const tagClasses = findCssClassesLazy("tagList", "tagGroup", "tag");
+const ServerProfileComponent = findComponentByCodeLazy("{guildProfile:", "GUILD_PROFILE");
+const getAttachmentType: (attachment: MessageAttachment, inlineAttachmentMedia?: boolean) => AttachmentType =
+    findByCodeLazy('"PLAINTEXT_PREVIEW":"OTHER"');
+
+export function GuildName({ guildId }: { guildId: string }) {
+    const guild = useStateFromStores(
+        [GuildStore, SelectedGuildStore],
+        () => {
+            const current = SelectedGuildStore.getGuildId();
+            return current !== guildId ? GuildStore.getGuild(guildId) : null;
+        },
+        [guildId]
+    );
+    const icon = useMemo(() => {
+        if (!guild?.icon) return null;
+        return IconUtils.getGuildIconURL({ id: guildId, icon: guild.icon, canAnimate: true, size: 16 });
+    }, [guildId, guild?.icon]);
+
+    const guildDivRef = useRef(null);
+
+    return (
+        guild && (
+            <Popout
+                position="top"
+                renderPopout={() => <ServerProfileComponent guildId={guildId} />}
+                targetElementRef={guildDivRef}
+            >
+                {popoutProps => (
+                    <div ref={guildDivRef} className={cl("footer-element")} {...popoutProps}>
+                        {icon && <img src={icon} alt={`Server icon for ${guild.name}`} className={cl("guild-icon")} />}
+                        <BaseText size="sm" weight="medium" className={cl("footer-text")}>
+                            {guild ? guild.name : "View server"}
+                        </BaseText>
+                        <RightArrow width={12} height={12} fill="var(--text-muted)" />
+                    </div>
+                )}
+            </Popout>
+        )
+    );
+}
+
+export function ChannelName({
+    guildId,
+    channelId,
+    messageId
+}: {
+    guildId?: string;
+    channelId: string;
+    messageId: string;
+}) {
+    const name = useStateFromStores(
+        [ChannelStore, UserStore],
+        () => {
+            const channel = ChannelStore.getChannel(channelId);
+            if (!channel) return null;
+
+            return match(channel.type)
+                .with(ChannelType.DM, () => {
+                    const user = UserStore.getUser(channel.recipients[0]);
+                    return `@${user.globalName || user.username}`;
+                })
+                .with(ChannelType.GROUP_DM, () => {
+                    if (channel.name) return channel.name;
+                    const users = channel.recipients.map(r => UserStore.getUser(r));
+                    return users.map(u => u.globalName || u.username).join(", ");
+                })
+                .with(
+                    ChannelType.ANNOUNCEMENT_THREAD,
+                    ChannelType.PRIVATE_THREAD,
+                    ChannelType.PUBLIC_THREAD,
+                    () => channel.name
+                )
+                .otherwise(() => `#${channel.name}`);
+        },
+        [channelId]
+    );
+
+    return (
+        name && (
+            <div
+                className={cl("footer-element")}
+                onClick={() => NavigationRouter.transitionTo(`/channels/${guildId ?? "@me"}/${channelId}/${messageId}`)}
+            >
+                <BaseText size="sm" weight="medium" className={cl("footer-text")}>
+                    {name}
+                </BaseText>
+                <RightArrow width={12} height={12} fill="var(--text-muted)" />
+            </div>
+        )
+    );
+}
+
+export function Timestamp({ snowflake }: { snowflake: string }) {
+    const formatted = useMemo(
+        () => DateUtils.calendarFormat(new Date(SnowflakeUtils.extractTimestamp(snowflake))),
+        [snowflake]
+    );
+
+    return (
+        <div className={cl("footer-element")} style={{ pointerEvents: "none" }}>
+            <BaseText size="sm" weight="medium" className={cl("footer-text")}>
+                {formatted}
+            </BaseText>
+        </div>
+    );
+}
+
+export function ForwardPicker({
+    message,
+    options,
+    onChange
+}: {
+    message: Message;
+    options: ForwardOptions;
+    onChange: Dispatch<SetStateAction<ForwardOptions>>;
+}) {
+    const textEnabled = !options.onlyAttachmentIds && !options.onlyEmbedIndices;
+    const embeds = useMemo(() => {
+        let id = 0;
+        return message.embeds.map(({ rawTitle, rawDescription, image, images = image ? [image] : [], video }, i) => {
+            const current = {
+                title: rawTitle?.trim() || rawDescription?.trim() || `Embed ${i}`,
+                subEmbeds: [] as { id: number; name: string }[]
+            };
+
+            if (images.length > 0) {
+                current.subEmbeds = images.map((image, i) => ({
+                    id: id++,
+                    name: `Image ${images.length > 1 ? `${i + 1} ` : ""}(${image!.width} x ${image!.height})`
+                }));
+            } else if (video) {
+                current.subEmbeds = [{ id: id++, name: "Video" }];
+            } else {
+                current.subEmbeds = [{ id: id++, name: "Embed" }];
+            }
+
+            return current;
+        });
+    }, [message]);
+
+    const defaultOpts = useMemo(
+        () => ({
+            onlyAttachmentIds: message.attachments.map(a => a.id),
+            onlyEmbedIndices: embeds.flatMap(e => e.subEmbeds.map(se => se.id))
+        }),
+        [message, embeds]
+    );
+
+    const { EmbedIcon, ChatIcon } = iconsModule;
+
+    return (
+        <Flex className={Margins.top8} gap={12} flexDirection="column">
+            <TagContainer>
+                <Tooltip text="Can be used when all embeds and attachments are enabled">
+                    {props => (
+                        <div
+                            className={tagClasses.tag}
+                            data-selection-mode="multiple"
+                            data-selected={textEnabled ? "true" : undefined}
+                            {...props}
+                            onClick={() => (onChange(textEnabled ? defaultOpts : {}), props.onClick())}
+                        >
+                            {ChatIcon && <ChatIcon size="xs" style={{ flexShrink: 0 }} />}
+                            <BaseText size="sm">Original message</BaseText>
+                        </div>
+                    )}
+                </Tooltip>
+                {message.attachments.map(attachment => (
+                    <Tag
+                        key={attachment.id}
+                        id={attachment.id}
+                        source={options.onlyAttachmentIds ?? defaultOpts.onlyAttachmentIds}
+                        onChange={data => onChange(prev => ({ ...prev, onlyAttachmentIds: data }))}
+                    >
+                        <AttachmentIcon attachment={attachment} />
+                        <BaseText size="sm">{attachment.filename}</BaseText>
+                    </Tag>
+                ))}
+            </TagContainer>
+            {embeds.map(({ title, subEmbeds }, i) => (
+                <Flex gap={4} flexDirection="column" key={i}>
+                    <BaseText
+                        size="sm"
+                        color="text-muted"
+                        style={{ textWrap: "nowrap", overflow: "hidden", textOverflow: "ellipsis" }}
+                    >
+                        {title}
+                    </BaseText>
+                    <TagContainer>
+                        {subEmbeds.map(({ id, name }) => (
+                            <Tag
+                                key={id}
+                                id={id}
+                                source={options.onlyEmbedIndices ?? defaultOpts.onlyEmbedIndices}
+                                onChange={data => onChange(prev => ({ ...prev, onlyEmbedIndices: data }))}
+                            >
+                                {EmbedIcon && <EmbedIcon size="xs" style={{ flexShrink: 0 }} />}
+                                <BaseText size="sm">{name}</BaseText>
+                            </Tag>
+                        ))}
+                    </TagContainer>
+                </Flex>
+            ))}
+        </Flex>
+    );
+}
+
+function TagContainer(props: FlexProps) {
+    return <Flex gap={8} flexWrap="wrap" className={tagClasses.tagGroup} data-layout="inline" {...props} />;
+}
+
+function Tag<T>({
+    id,
+    children,
+    source,
+    onChange
+}: { id: T; source: T[]; onChange: (data: T[]) => void } & PropsWithChildren) {
+    const selected = useMemo(() => source.includes(id), [source, id]);
+
+    return (
+        <div
+            className={tagClasses.tag}
+            data-selection-mode="multiple"
+            data-selected={selected ? "true" : undefined}
+            onClick={() => onChange(selected ? source.filter(x => x !== id) : [...source, id])}
+            style={{ textWrap: "wrap" }}
+        >
+            {children}
+        </div>
+    );
+}
+
+const icons: Partial<Record<AttachmentType, string>> = {
+    IMAGE: "Image",
+    VIDEO: "Video",
+    CLIP: "Clips",
+    AUDIO: "Music",
+    PLAINTEXT_PREVIEW: "A"
+};
+
+function AttachmentIcon({ attachment }: { attachment: MessageAttachment }) {
+    const Icon = useMemo(() => {
+        const type = getAttachmentType(attachment, true);
+        return iconsModule[(icons[type] ?? "ImageFile") + "Icon"];
+    }, [attachment]);
+
+    return Icon && <Icon size="xs" style={{ flexShrink: 0 }} />;
+}
---
+++ src/equicordplugins/betterForwards/index.tsx
@@ -0,0 +1,216 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { definePluginSettings, migratePluginSettings } from "@api/Settings";
+import { Devs, EquicordDevs } from "@utils/constants";
+import { sendMessage } from "@utils/discord";
+import definePlugin, { OptionType } from "@utils/types";
+import { Message } from "@vencord/discord-types";
+import { Dispatch, MouseEvent, SetStateAction } from "react";
+
+export const cl = classNameFactory("vc-betterforwards-");
+
+import { classNameFactory } from "@utils/css";
+import { ChannelActionCreators, ChannelStore, useState } from "@webpack/common";
+
+import { ChannelName, ForwardPicker, GuildName, Timestamp } from "./components";
+import managedStyle from "./style.css?managed";
+
+export interface ForwardOptions {
+    onlyEmbedIndices?: number[];
+    onlyAttachmentIds?: string[];
+}
+
+let ignore = false;
+const getId = ({ id, type }: { id: string; type: string }) => {
+    if (type !== "user") return id;
+    return (
+        ChannelStore.getDMFromUserId(id) ??
+        (ChannelActionCreators.getOrEnsurePrivateChannel(id) as Promise<string | void>)
+    );
+};
+
+// Taken From Signature :)
+const settings = definePluginSettings({
+    resendOnFail: {
+        description:
+            "This will attempt to resend a forwarded message if the forward fails. Could cause unintentional pings or text spam. Bypasses NSFW restrictions.",
+        type: OptionType.BOOLEAN,
+        default: true,
+        restartNeeded: true
+    },
+    forwardPreface: {
+        description: "What should forwarded from be prefaced with",
+        type: OptionType.SELECT,
+        hidden: () => !settings.store.resendOnFail,
+        options: [
+            { label: ">", value: ">", default: true },
+            { label: "-#", value: "-#" }
+        ]
+    },
+    dontFollowForwards: {
+        description: "After forwarding a single message, don't jump to it. Hold shift to ignore this behavior.",
+        displayName: "Don't Follow Forwards",
+        type: OptionType.BOOLEAN,
+        default: false,
+        restartNeeded: true
+    },
+    selfForward: {
+        description: "Show the current channel in the forward list popup.",
+        type: OptionType.BOOLEAN,
+        default: false,
+        restartNeeded: true
+    }
+});
+
+migratePluginSettings("BetterForwards", "ForwardAnywhere");
+export default definePlugin({
+    name: "BetterForwards",
+    description: "Message forward utilities including NSFW bypass and UI improvements.",
+    tags: ["Chat", "Utility"],
+    searchTerms: ["selfForward", "betterForwardMeta"],
+    authors: [Devs.thororen, Devs.sadan, Devs.nin0dev, EquicordDevs.VillainsRule, EquicordDevs.davri],
+    settings,
+    managedStyle,
+    patches: [
+        {
+            find: "#{intl::MESSAGE_FORWARDING_NSFW_NOT_ALLOWED}",
+            predicate: () => settings.store.resendOnFail,
+            replacement: {
+                match: /(\{if\().{0,50}(\)return.{0,25}#{intl::MESSAGE_FORWARDING_NSFW_NOT_ALLOWED})/,
+                replace: "$1false$2"
+            }
+        },
+        {
+            find: "#{intl::MESSAGE_ACTION_FORWARD_TO}",
+            replacement: [
+                {
+                    match: /(?<=let (\i)=.{0,25}rejected.{0,25}\);)(?=.{0,25}message:(\i))/,
+                    replace: "if($1.length>0)return await $self.sendForward($1,$2,__state.opts);",
+                    predicate: () => settings.store.resendOnFail
+                },
+                {
+                    match: /(?<=source:\i,)\.\.\.(\i)\}=(\i),/,
+                    replace: "__state,...$1}=$self.useProps($2),"
+                },
+                {
+                    match: /\{message:(\i),forwardOptions:\i,channel:\i\}\),/,
+                    replace: "$&$self.renderPicker($1,__state),"
+                },
+                {
+                    match: /(?<=transitionToDestination:)(1===\i\.length)(?=,|\})/,
+                    replace: "$self.shouldTransition($1)",
+                    predicate: () => settings.store.dontFollowForwards
+                },
+                {
+                    // there are two useCallbacks with clearDraft in this module
+                    // we need to anchor to the one that is used as an onClick handler
+                    match: /((\i)=\i\.useCallback\(\()(\)=>\{)(null!=\i&&\i\.\i\.clearDraft)(?=.{1500,2000}onClick:\2)/,
+                    replace: (_, beforeParen, _1, beforeBody, body) =>
+                        `${beforeParen}vencordArg1${beforeBody}$self.setShift(vencordArg1);${body}`,
+                    predicate: () => settings.store.dontFollowForwards
+                }
+            ]
+        },
+        {
+            find: 'location:"ForwardFooter"',
+            replacement: {
+                match: /let{message:\i,snapshot:\i,index:\i}=(\i)/,
+                replace: "return $self.renderForwardFooter($1);$&"
+            }
+        },
+        {
+            find: ".getChannelHistory(),",
+            predicate: () => settings.store.selfForward,
+            replacement: {
+                match: /\i.id\]/,
+                replace: "]"
+            }
+        }
+    ],
+
+    async sendForward(channels: { id: string; type: string }[], message: Message, options: ForwardOptions) {
+        const contentMessage = message.messageSnapshots[0]?.message ?? message;
+
+        const attachments = options.onlyAttachmentIds
+            ? contentMessage.attachments.filter(a => options.onlyAttachmentIds!.includes(a.id))
+            : contentMessage.attachments;
+
+        const ids = (await Promise.all(channels.map(getId))).filter(Boolean) as string[];
+
+        const chunkSize = 5;
+        ids.forEach(id => {
+            if (attachments.length) {
+                for (let i = 0; i < attachments.length; i += chunkSize) {
+                    const group = attachments.slice(i, i + chunkSize);
+
+                    let text = i === 0 ? `${contentMessage.content}\nAttachments:\n` : "";
+                    text += `${group.map(a => a.url).join("\n")}\n`;
+                    if (i + chunkSize >= attachments.length)
+                        text += `${settings.store.forwardPreface} Forwarded from <#${message.channel_id}>`;
+
+                    sendMessage(id, { content: text });
+                }
+            } else {
+                sendMessage(id, {
+                    content: `${contentMessage.content}\n${settings.store.forwardPreface} Forwarded from <#${message.channel_id}>`
+                });
+            }
+        });
+    },
+
+    shouldTransition(origCond: boolean): boolean {
+        return ignore ? origCond : false;
+    },
+
+    setShift(event: MouseEvent | undefined) {
+        ignore = !!event?.shiftKey;
+    },
+
+    renderForwardFooter({ message }: { message: Message }) {
+        const { guild_id, channel_id, message_id } = message.messageReference!;
+        return (
+            <div className={cl("footer")}>
+                {guild_id && <GuildName guildId={guild_id} />}
+                <ChannelName messageId={message_id} channelId={channel_id} guildId={guild_id} />
+                <Timestamp snowflake={message_id} />
+            </div>
+        );
+    },
+
+    useProps(props: { message: Message; forwardOptions?: ForwardOptions }) {
+        const [opts, setOpts] = useState(() => {
+            if (!props.forwardOptions || !props.forwardOptions.onlyEmbedIndices)
+                return props.forwardOptions ?? ({} as ForwardOptions);
+
+            let id = 0;
+            const embedsIds = new Set(props.forwardOptions.onlyEmbedIndices as number[]);
+
+            // Discord incorrectly assumes that embed indices directly map to whole embeds, this is an attempt to fix that
+            const onlyEmbedIndices = props.message.embeds
+                .flatMap((e, i) => e.images?.map(() => ({ id: id++, eId: i })) ?? { id: id++, eId: i })
+                .filter(({ eId }) => embedsIds.has(eId))
+                .map(({ id }) => id);
+
+            return { ...props.forwardOptions, onlyEmbedIndices };
+        });
+
+        const forwardOptions: ForwardOptions = { ...opts };
+        const hasOpts = !!opts.onlyAttachmentIds || !!opts.onlyEmbedIndices;
+
+        // Server-side validation can be bypassed by specifying a fake attachment id
+        if (hasOpts && !forwardOptions.onlyAttachmentIds?.length) forwardOptions.onlyAttachmentIds = ["0"];
+
+        return { ...props, forwardOptions, __state: { opts, setOpts } };
+    },
+
+    renderPicker(message: Message, state: { opts: ForwardOptions; setOpts: Dispatch<SetStateAction<ForwardOptions>> }) {
+        const contentMessage = message.messageSnapshots[0]?.message ?? message;
+        if (contentMessage.embeds.length === 0 && contentMessage.attachments.length === 0) return null;
+
+        return <ForwardPicker message={contentMessage} options={state.opts} onChange={state.setOpts} />;
+    }
+});
