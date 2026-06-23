f461b6588c4b0be70c0325293faad62b4b56fee0
nin0 <nin0@nin0.dev>
2026-06-23 17:10:03 -0400

    add safety icons

--- src/equicordplugins/userpluginLibrary.dev/components/LibraryTab.tsx
+++ src/equicordplugins/userpluginLibrary.dev/components/LibraryTab.tsx
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import { Button } from "@components/Button";
 import { Flex } from "@components/Flex";
 import { Link } from "@components/Link";
 import { Notice } from "@components/Notice";
@@ -14,7 +13,7 @@ import {
 } from "@components/settings/tabs/BaseTab";
 
 import { OpenSettingsModule } from "..";
-import { cl } from "../misc/constants";
+import { cl, SafetyIconsButton } from "../misc/constants";
 
 function LibraryTab() {
     return <STab>
@@ -24,8 +23,10 @@ function LibraryTab() {
             </Notice.Warning>
             <Notice.Info>
                 <ul className={cl("unordered-list")}>
-                    <li>All UserPlugins in this section have been manually reviewed and are known to be safe to use. You can look at the safety icons next to plugin names for more info.
-                        <Button variant="secondary" style={{ marginTop: "5px" }}>What does each icon mean?</Button></li>
+                    <li>
+                        All UserPlugins in this section have been manually reviewed and are known to be safe to use. You can look at the safety icons next to plugin names for more info.
+                        <SafetyIconsButton />
+                    </li>
                     <li>Looking for your installed UserPlugins? Go in the <Link onClick={() => OpenSettingsModule.openUserSettings("vencord_userplugins_panel")}>UserPlugins</Link> settings section.</li>
                 </ul>
 
