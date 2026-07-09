6e6313fe1f49447a1e42ca1b8da061ca3d6e6106
Star123451 <kadena775@gmail.com>
2026-07-08 21:49:04 -0500

    chore(richPrecence): switch to ts-md5 source for a robust MD5 implementation

--- src/equicordplugins/richPresence/utils.ts
+++ src/equicordplugins/richPresence/utils.ts
@@ -3,185 +3,309 @@
  * Copyright (c) 2026 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
- * MD5 implementation adapted from https://github.com/blueimp/JavaScript-MD5
- * Copyright 2011, Sebastian Tschan, MIT License
+ * MD5 implementation adapted from https://github.com/cotag/ts-md5
+ * Copyright (C) 2014 Raymond Hill, MIT License
  */
-// @ts-nocheck
 
-function safeAdd(x, y) {
-  var lsw = (x & 0xffff) + (y & 0xffff)
-  var msw = (x >> 16) + (y >> 16) + (lsw >> 16)
-  return (msw << 16) | (lsw & 0xffff)
+interface HasherState {
+    buffer: string;
+    buflen: number;
+    length: number;
+    state: number[];
 }
 
-function bitRotateLeft(num, cnt) {
-  return (num << cnt) | (num >>> (32 - cnt))
+const EMPTY_STATE = new Int32Array(4);
+
+export class Md5 {
+    public static hashStr(str: string, raw?: false): string;
+    public static hashStr(str: string, raw: true): Int32Array;
+    public static hashStr(str: string, raw: boolean = false) {
+        return this.onePassHasher.start().appendStr(str).end(raw);
+    }
+
+    private static stateIdentity = new Int32Array([
+        1732584193, -271733879, -1732584194, 271733878,
+    ]);
+    private static buffer32Identity = new Int32Array([
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ]);
+    private static hexChars = '0123456789abcdef';
+    private static hexOut: string[] = [];
+
+    private static onePassHasher = new Md5();
+
+    private static _hex(x: Int32Array): string {
+        const hc = Md5.hexChars;
+        const ho = Md5.hexOut;
+        let n;
+        let offset;
+        let j;
+        let i;
+
+        for (i = 0; i < 4; i += 1) {
+            offset = i * 8;
+            n = x[i];
+            for (j = 0; j < 8; j += 2) {
+                ho[offset + 1 + j] = hc.charAt(n & 0x0f);
+                n >>>= 4;
+                ho[offset + 0 + j] = hc.charAt(n & 0x0f);
+                n >>>= 4;
+            }
+        }
+        return ho.join('');
+    }
+
+    private static _md5cycle(
+        x: Int32Array | Uint32Array,
+        k: Int32Array | Uint32Array,
+    ) {
+        let a = x[0];
+        let b = x[1];
+        let c = x[2];
+        let d = x[3];
+        a += (((b & c) | (~b & d)) + k[0] - 680876936) | 0;
+        a = (((a << 7) | (a >>> 25)) + b) | 0;
+        d += (((a & b) | (~a & c)) + k[1] - 389564586) | 0;
+        d = (((d << 12) | (d >>> 20)) + a) | 0;
+        c += (((d & a) | (~d & b)) + k[2] + 606105819) | 0;
+        c = (((c << 17) | (c >>> 15)) + d) | 0;
+        b += (((c & d) | (~c & a)) + k[3] - 1044525330) | 0;
+        b = (((b << 22) | (b >>> 10)) + c) | 0;
+
+        a += (((b & c) | (~b & d)) + k[4] - 176418897) | 0;
+        a = (((a << 7) | (a >>> 25)) + b) | 0;
+        d += (((a & b) | (~a & c)) + k[5] + 1200080426) | 0;
+        d = (((d << 12) | (d >>> 20)) + a) | 0;
+        c += (((d & a) | (~d & b)) + k[6] - 1473231341) | 0;
+        c = (((c << 17) | (c >>> 15)) + d) | 0;
+        b += (((c & d) | (~c & a)) + k[7] - 45705983) | 0;
+        b = (((b << 22) | (b >>> 10)) + c) | 0;
+        a += (((b & c) | (~b & d)) + k[8] + 1770035416) | 0;
+        a = (((a << 7) | (a >>> 25)) + b) | 0;
+        d += (((a & b) | (~a & c)) + k[9] - 1958414417) | 0;
+        d = (((d << 12) | (d >>> 20)) + a) | 0;
+        c += (((d & a) | (~d & b)) + k[10] - 42063) | 0;
+        c = (((c << 17) | (c >>> 15)) + d) | 0;
+        b += (((c & d) | (~c & a)) + k[11] - 1990404162) | 0;
+        b = (((b << 22) | (b >>> 10)) + c) | 0;
+        a += (((b & c) | (~b & d)) + k[12] + 1804603682) | 0;
+        a = (((a << 7) | (a >>> 25)) + b) | 0;
+        d += (((a & b) | (~a & c)) + k[13] - 40341101) | 0;
+        d = (((d << 12) | (d >>> 20)) + a) | 0;
+        c += (((d & a) | (~d & b)) + k[14] - 1502002290) | 0;
+        c = (((c << 17) | (c >>> 15)) + d) | 0;
+        b += (((c & d) | (~c & a)) + k[15] + 1236535329) | 0;
+        b = (((b << 22) | (b >>> 10)) + c) | 0;
+        
+        a += (((b & d) | (c & ~d)) + k[1] - 165796510) | 0;
+        a = (((a << 5) | (a >>> 27)) + b) | 0;
+        d += (((a & c) | (b & ~c)) + k[6] - 1069501632) | 0;
+        d = (((d << 9) | (d >>> 23)) + a) | 0;
+        c += (((d & b) | (a & ~b)) + k[11] + 643717713) | 0;
+        c = (((c << 14) | (c >>> 18)) + d) | 0;
+        b += (((c & a) | (d & ~a)) + k[0] - 373897302) | 0;
+        b = (((b << 20) | (b >>> 12)) + c) | 0;
+        a += (((b & d) | (c & ~d)) + k[5] - 701558691) | 0;
+        a = (((a << 5) | (a >>> 27)) + b) | 0;
+        d += (((a & c) | (b & ~c)) + k[10] + 38016083) | 0;
+        d = (((d << 9) | (d >>> 23)) + a) | 0;
+        c += (((d & b) | (a & ~b)) + k[15] - 660478335) | 0;
+        c = (((c << 14) | (c >>> 18)) + d) | 0;
+        b += (((c & a) | (d & ~a)) + k[4] - 405537848) | 0;
+        b = (((b << 20) | (b >>> 12)) + c) | 0;
+        a += (((b & d) | (c & ~d)) + k[9] + 568446438) | 0;
+        a = (((a << 5) | (a >>> 27)) + b) | 0;
+        d += (((a & c) | (b & ~c)) + k[14] - 1019803690) | 0;
+        d = (((d << 9) | (d >>> 23)) + a) | 0;
+        c += (((d & b) | (a & ~b)) + k[3] - 187363961) | 0;
+        c = (((c << 14) | (c >>> 18)) + d) | 0;
+        b += (((c & a) | (d & ~a)) + k[8] + 1163531501) | 0;
+        b = (((b << 20) | (b >>> 12)) + c) | 0;
+        a += (((b & d) | (c & ~d)) + k[13] - 1444681467) | 0;
+        a = (((a << 5) | (a >>> 27)) + b) | 0;
+        d += (((a & c) | (b & ~c)) + k[2] - 51403784) | 0;
+        d = (((d << 9) | (d >>> 23)) + a) | 0;
+        c += (((d & b) | (a & ~b)) + k[7] + 1735328473) | 0;
+        c = (((c << 14) | (c >>> 18)) + d) | 0;
+        b += (((c & a) | (d & ~a)) + k[12] - 1926607734) | 0;
+        b = (((b << 20) | (b >>> 12)) + c) | 0;
+        
+        a += ((b ^ c ^ d) + k[5] - 378558) | 0;
+        a = (((a << 4) | (a >>> 28)) + b) | 0;
+        d += ((a ^ b ^ c) + k[8] - 2022574463) | 0;
+        d = (((d << 11) | (d >>> 21)) + a) | 0;
+        c += ((d ^ a ^ b) + k[11] + 1839030562) | 0;
+        c = (((c << 16) | (c >>> 16)) + d) | 0;
+        b += ((c ^ d ^ a) + k[14] - 35309556) | 0;
+        b = (((b << 23) | (b >>> 9)) + c) | 0;
+        a += ((b ^ c ^ d) + k[1] - 1530992060) | 0;
+        a = (((a << 4) | (a >>> 28)) + b) | 0;
+        d += ((a ^ b ^ c) + k[4] + 1272893353) | 0;
+        d = (((d << 11) | (d >>> 21)) + a) | 0;
+        c += ((d ^ a ^ b) + k[7] - 155497632) | 0;
+        c = (((c << 16) | (c >>> 16)) + d) | 0;
+        b += ((c ^ d ^ a) + k[10] - 1094730640) | 0;
+        b = (((b << 23) | (b >>> 9)) + c) | 0;
+        a += ((b ^ c ^ d) + k[13] + 681279174) | 0;
+        a = (((a << 4) | (a >>> 28)) + b) | 0;
+        d += ((a ^ b ^ c) + k[0] - 358537222) | 0;
+        d = (((d << 11) | (d >>> 21)) + a) | 0;
+        c += ((d ^ a ^ b) + k[3] - 722521979) | 0;
+        c = (((c << 16) | (c >>> 16)) + d) | 0;
+        b += ((c ^ d ^ a) + k[6] + 76029189) | 0;
+        b = (((b << 23) | (b >>> 9)) + c) | 0;
+        a += ((b ^ c ^ d) + k[9] - 640364487) | 0;
+        a = (((a << 4) | (a >>> 28)) + b) | 0;
+        d += ((a ^ b ^ c) + k[12] - 421815835) | 0;
+        d = (((d << 11) | (d >>> 21)) + a) | 0;
+        c += ((d ^ a ^ b) + k[15] + 530742520) | 0;
+
+        c = (((c << 16) | (c >>> 16)) + d) | 0;
+        b += ((c ^ d ^ a) + k[2] - 995338651) | 0;
+        b = (((b << 23) | (b >>> 9)) + c) | 0;
+        
+        a += ((c ^ (b | ~d)) + k[0] - 198630844) | 0;
+        a = (((a << 6) | (a >>> 26)) + b) | 0;
+        d += ((b ^ (a | ~c)) + k[7] + 1126891415) | 0;
+        d = (((d << 10) | (d >>> 22)) + a) | 0;
+        c += ((a ^ (d | ~b)) + k[14] - 1416354905) | 0;
+        c = (((c << 15) | (c >>> 17)) + d) | 0;
+        b += ((d ^ (c | ~a)) + k[5] - 57434055) | 0;
+        b = (((b << 21) | (b >>> 11)) + c) | 0;
+        a += ((c ^ (b | ~d)) + k[12] + 1700485571) | 0;
+        a = (((a << 6) | (a >>> 26)) + b) | 0;
+        d += ((b ^ (a | ~c)) + k[3] - 1894986606) | 0;
+        d = (((d << 10) | (d >>> 22)) + a) | 0;
+        c += ((a ^ (d | ~b)) + k[10] - 1051523) | 0;
+        c = (((c << 15) | (c >>> 17)) + d) | 0;
+        b += ((d ^ (c | ~a)) + k[1] - 2054922799) | 0;
+        b = (((b << 21) | (b >>> 11)) + c) | 0;
+        a += ((c ^ (b | ~d)) + k[8] + 1873313359) | 0;
+        a = (((a << 6) | (a >>> 26)) + b) | 0;
+        d += ((b ^ (a | ~c)) + k[15] - 30611744) | 0;
+        d = (((d << 10) | (d >>> 22)) + a) | 0;
+        c += ((a ^ (d | ~b)) + k[6] - 1560198380) | 0;
+        c = (((c << 15) | (c >>> 17)) + d) | 0;
+        b += ((d ^ (c | ~a)) + k[13] + 1309151649) | 0;
+        b = (((b << 21) | (b >>> 11)) + c) | 0;
+        a += ((c ^ (b | ~d)) + k[4] - 145523070) | 0;
+        a = (((a << 6) | (a >>> 26)) + b) | 0;
+        d += ((b ^ (a | ~c)) + k[11] - 1120210379) | 0;
+        d = (((d << 10) | (d >>> 22)) + a) | 0;
+        c += ((a ^ (d | ~b)) + k[2] + 718787259) | 0;
+        c = (((c << 15) | (c >>> 17)) + d) | 0;
+        b += ((d ^ (c | ~a)) + k[9] - 343485551) | 0;
+        b = (((b << 21) | (b >>> 11)) + c) | 0;
+
+        x[0] = (a + x[0]) | 0;
+        x[1] = (b + x[1]) | 0;
+        x[2] = (c + x[2]) | 0;
+        x[3] = (d + x[3]) | 0;
+    }
+
+    private _dataLength = 0;
+    private _bufferLength = 0;
+
+    private _state: Int32Array = new Int32Array(4);
+    private _buffer: ArrayBuffer = new ArrayBuffer(68);
+    private _buffer8: Uint8Array;
+    private _buffer32: Uint32Array;
+
+    constructor() {
+        this._buffer8 = new Uint8Array(this._buffer, 0, 68);
+        this._buffer32 = new Uint32Array(this._buffer, 0, 17);
+        this.start();
+    }
+
+    public start() {
+        this._dataLength = 0;
+        this._bufferLength = 0;
+        this._state.set(Md5.stateIdentity);
+        return this;
+    }
+
+    public appendStr(str: string) {
+        const buf8 = this._buffer8;
+        const buf32 = this._buffer32;
+        let bufLen = this._bufferLength;
+        let code;
+        let i;
+
+        for (i = 0; i < str.length; i += 1) {
+            code = str.charCodeAt(i);
+            if (code < 128) {
+                buf8[bufLen++] = code;
+            } else if (code < 0x800) {
+                buf8[bufLen++] = (code >>> 6) + 0xc0;
+                buf8[bufLen++] = (code & 0x3f) | 0x80;
+            } else if (code < 0xd800 || code > 0xdbff) {
+                buf8[bufLen++] = (code >>> 12) + 0xe0;
+                buf8[bufLen++] = ((code >>> 6) & 0x3f) | 0x80;
+                buf8[bufLen++] = (code & 0x3f) | 0x80;
+            } else {
+                code =
+                    (code - 0xd800) * 0x400 +
+                    (str.charCodeAt(++i) - 0xdc00) +
+                    0x10000;
+                if (code > 0x10ffff) {
+                    throw new Error(
+                        'Unicode standard supports code points up to U+10FFFF',
+                    );
+                }
+                buf8[bufLen++] = (code >>> 18) + 0xf0;
+                buf8[bufLen++] = ((code >>> 12) & 0x3f) | 0x80;
+                buf8[bufLen++] = ((code >>> 6) & 0x3f) | 0x80;
+                buf8[bufLen++] = (code & 0x3f) | 0x80;
+            }
+            if (bufLen >= 64) {
+                this._dataLength += 64;
+                Md5._md5cycle(this._state, buf32);
+                bufLen -= 64;
+                buf32[0] = buf32[16];
+            }
+        }
+
+        this._bufferLength = bufLen;
+        return this;
+    }
+
+    public end(raw: boolean = false): Int32Array | string {
+        const bufLen = this._bufferLength;
+        const buf8 = this._buffer8;
+        const buf32 = this._buffer32;
+        const i = (bufLen >> 2) + 1;
+
+        this._dataLength += bufLen;
+        const dataBitsLen = this._dataLength * 8;
+
+        buf8[bufLen] = 0x80;
+        buf8[bufLen + 1] = buf8[bufLen + 2] = buf8[bufLen + 3] = 0;
+        buf32.set(Md5.buffer32Identity.subarray(i), i);
+
+        if (bufLen > 55) {
+            Md5._md5cycle(this._state, buf32);
+            buf32.set(Md5.buffer32Identity);
+        }
+
+        if (dataBitsLen <= 0xffffffff) {
+            buf32[14] = dataBitsLen;
+        } else {
+            const matches = dataBitsLen.toString(16).match(/(.*?)(.{0,8})$/);
+            if (matches === null) return raw ? EMPTY_STATE : '';
+
+            const lo = parseInt(matches[2], 16);
+            const hi = parseInt(matches[1], 16) || 0;
+
+            buf32[14] = lo;
+            buf32[15] = hi;
+        }
+
+        Md5._md5cycle(this._state, buf32);
+
+        return raw ? this._state : Md5._hex(this._state);
+    }
 }
 
-function md5cmn(q, a, b, x, s, t) {
-  return safeAdd(bitRotateLeft(safeAdd(safeAdd(a, q), safeAdd(x, t)), s), b)
-}
-function md5ff(a, b, c, d, x, s, t) {
-  return md5cmn((b & c) | (~b & d), a, b, x, s, t)
-}
-function md5gg(a, b, c, d, x, s, t) {
-  return md5cmn((b & d) | (c & ~d), a, b, x, s, t)
-}
-function md5hh(a, b, c, d, x, s, t) {
-  return md5cmn(b ^ c ^ d, a, b, x, s, t)
-}
-function md5ii(a, b, c, d, x, s, t) {
-  return md5cmn(c ^ (b | ~d), a, b, x, s, t)
-}
-
-function binlMD5(x, len) {
-  x[len >> 5] |= 0x80 << len % 32
-  x[(((len + 64) >>> 9) << 4) + 14] = len
-
-  var i
-  var olda
-  var oldb
-  var oldc
-  var oldd
-  var a = 1732584193
-  var b = -271733879
-  var c = -1732584194
-  var d = 271733878
-
-  for (i = 0; i < x.length; i += 16) {
-    olda = a
-    oldb = b
-    oldc = c
-    oldd = d
-
-    a = md5ff(a, b, c, d, x[i], 7, -680876936)
-    d = md5ff(d, a, b, c, x[i + 1], 12, -389564586)
-    c = md5ff(c, d, a, b, x[i + 2], 17, 606105819)
-    b = md5ff(b, c, d, a, x[i + 3], 22, -1044525330)
-    a = md5ff(a, b, c, d, x[i + 4], 7, -176418897)
-    d = md5ff(d, a, b, c, x[i + 5], 12, 1200080426)
-    c = md5ff(c, d, a, b, x[i + 6], 17, -1473231341)
-    b = md5ff(b, c, d, a, x[i + 7], 22, -45705983)
-    a = md5ff(a, b, c, d, x[i + 8], 7, 1770035416)
-    d = md5ff(d, a, b, c, x[i + 9], 12, -1958414417)
-    c = md5ff(c, d, a, b, x[i + 10], 17, -42063)
-    b = md5ff(b, c, d, a, x[i + 11], 22, -1990404162)
-    a = md5ff(a, b, c, d, x[i + 12], 7, 1804603682)
-    d = md5ff(d, a, b, c, x[i + 13], 12, -40341101)
-    c = md5ff(c, d, a, b, x[i + 14], 17, -1502002290)
-    b = md5ff(b, c, d, a, x[i + 15], 22, 1236535329)
-
-    a = md5gg(a, b, c, d, x[i + 1], 5, -165796510)
-    d = md5gg(d, a, b, c, x[i + 6], 9, -1069501632)
-    c = md5gg(c, d, a, b, x[i + 11], 14, 643717713)
-    b = md5gg(b, c, d, a, x[i], 20, -373897302)
-    a = md5gg(a, b, c, d, x[i + 5], 5, -701558691)
-    d = md5gg(d, a, b, c, x[i + 10], 9, 38016083)
-    c = md5gg(c, d, a, b, x[i + 15], 14, -660478335)
-    b = md5gg(b, c, d, a, x[i + 4], 20, -405537848)
-    a = md5gg(a, b, c, d, x[i + 9], 5, 568446438)
-    d = md5gg(d, a, b, c, x[i + 14], 9, -1019803690)
-    c = md5gg(c, d, a, b, x[i + 3], 14, -187363961)
-    b = md5gg(b, c, d, a, x[i + 8], 20, 1163531501)
-    a = md5gg(a, b, c, d, x[i + 13], 5, -1444681467)
-    d = md5gg(d, a, b, c, x[i + 2], 9, -51403784)
-    c = md5gg(c, d, a, b, x[i + 7], 14, 1735328473)
-    b = md5gg(b, c, d, a, x[i + 12], 20, -1926607734)
-
-    a = md5hh(a, b, c, d, x[i + 5], 4, -378558)
-    d = md5hh(d, a, b, c, x[i + 8], 11, -2022574463)
-    c = md5hh(c, d, a, b, x[i + 11], 16, 1839030562)
-    b = md5hh(b, c, d, a, x[i + 14], 23, -35309556)
-    a = md5hh(a, b, c, d, x[i + 1], 4, -1530992060)
-    d = md5hh(d, a, b, c, x[i + 4], 11, 1272893353)
-    c = md5hh(c, d, a, b, x[i + 7], 16, -155497632)
-    b = md5hh(b, c, d, a, x[i + 10], 23, -1094730640)
-    a = md5hh(a, b, c, d, x[i + 13], 4, 681279174)
-    d = md5hh(d, a, b, c, x[i], 11, -358537222)
-    c = md5hh(c, d, a, b, x[i + 3], 16, -722521979)
-    b = md5hh(b, c, d, a, x[i + 6], 23, 76029189)
-    a = md5hh(a, b, c, d, x[i + 9], 4, -640364487)
-    d = md5hh(d, a, b, c, x[i + 12], 11, -421815835)
-    c = md5hh(c, d, a, b, x[i + 15], 16, 530742520)
-    b = md5hh(b, c, d, a, x[i + 2], 23, -995338651)
-
-    a = md5ii(a, b, c, d, x[i], 6, -198630844)
-    d = md5ii(d, a, b, c, x[i + 7], 10, 1126891415)
-    c = md5ii(c, d, a, b, x[i + 14], 15, -1416354905)
-    b = md5ii(b, c, d, a, x[i + 5], 21, -57434055)
-    a = md5ii(a, b, c, d, x[i + 12], 6, 1700485571)
-    d = md5ii(d, a, b, c, x[i + 3], 10, -1894986606)
-    c = md5ii(c, d, a, b, x[i + 10], 15, -1051523)
-    b = md5ii(b, c, d, a, x[i + 1], 21, -2054922799)
-    a = md5ii(a, b, c, d, x[i + 8], 6, 1873313359)
-    d = md5ii(d, a, b, c, x[i + 15], 10, -30611744)
-    c = md5ii(c, d, a, b, x[i + 6], 15, -1560198380)
-    b = md5ii(b, c, d, a, x[i + 13], 21, 1309151649)
-    a = md5ii(a, b, c, d, x[i + 4], 6, -145523070)
-    d = md5ii(d, a, b, c, x[i + 11], 10, -1120210379)
-    c = md5ii(c, d, a, b, x[i + 2], 15, 718787259)
-    b = md5ii(b, c, d, a, x[i + 9], 21, -343485551)
-
-    a = safeAdd(a, olda)
-    b = safeAdd(b, oldb)
-    c = safeAdd(c, oldc)
-    d = safeAdd(d, oldd)
-  }
-  return [a, b, c, d]
-}
-
-function binl2rstr(input) {
-  var i
-  var output = ''
-  var length32 = input.length * 32
-  for (i = 0; i < length32; i += 8) {
-    output += String.fromCharCode((input[i >> 5] >>> i % 32) & 0xff)
-  }
-  return output
-}
-
-function rstr2binl(input) {
-  var i
-  var output = []
-  output[(input.length >> 2) - 1] = undefined
-  for (i = 0; i < output.length; i += 1) {
-    output[i] = 0
-  }
-  var length8 = input.length * 8
-  for (i = 0; i < length8; i += 8) {
-    output[i >> 5] |= (input.charCodeAt(i / 8) & 0xff) << i % 32
-  }
-  return output
-}
-
-function rstrMD5(s) {
-  return binl2rstr(binlMD5(rstr2binl(s), s.length * 8))
-}
-
-function rstr2hex(input) {
-  var hexTab = '0123456789abcdef'
-  var output = ''
-  var x
-  var i
-  for (i = 0; i < input.length; i += 1) {
-    x = input.charCodeAt(i)
-    output += hexTab.charAt((x >>> 4) & 0x0f) + hexTab.charAt(x & 0x0f)
-  }
-  return output
-}
-
-function str2rstrUTF8(input) {
-  return unescape(encodeURIComponent(input))
-}
-
-function rawMD5(s) {
-  return rstrMD5(str2rstrUTF8(s))
-}
-
-function hexMD5(s) {
-  return rstr2hex(rawMD5(s))
-}
-
-export function md5(string) {
-  return hexMD5(string)
+export function md5(message: string): string {
+    return Md5.hashStr(message) as string;
 }
