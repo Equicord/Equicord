e5bf288834980373608711c4eeb766ca3cff7312
thororen1234 <78185467+thororen1234@users.noreply.github.com>
2026-08-05 22:34:32 -0400

    autojumptomessage fixes & actually ignore muted

--- src/equicordplugins/autoJumpToMessage/index.ts
+++ src/equicordplugins/autoJumpToMessage/index.ts
@@ -7,54 +7,28 @@
 import { definePluginSettings } from "@api/Settings";
 import { EquicordDevs } from "@utils/constants";
 import definePlugin, { OptionType } from "@utils/types";
-import { ChannelRouter, FluxDispatcher } from "@webpack/common";
+import { ChannelRouter, ChannelStore, UserGuildSettingsStore } from "@webpack/common";
 
 const settings = definePluginSettings({
     onlyWhenUnfocused: {
         type: OptionType.BOOLEAN,
         default: true,
-        description:
-            "Only automatically jump to messages when Discord's window is unfocused.",
+        description: "Only automatically jump to messages when Discord's window is unfocused.",
     },
 });
 
 export default definePlugin({
     name: "AutoJumpToMessage",
-    description:
-        "Automatically opens the channel from new messages. Muted channels are ignored. Made for those who keep Discord on another monitor.",
+    description: "Automatically opens the channel from new messages.",
     tags: ["Chat", "Utility"],
     searchTerms: ["jump", "message", "auto", "notification", "focus", "unfocused"],
     authors: [EquicordDevs.k304],
     settings,
-
-    handleNotification(payload: any) {
-        if (!payload) return;
-
-        if (payload.type === "RPC_NOTIFICATION_CREATE") {
-            // Dont do anything if discord is in focus
-            if (settings.store.onlyWhenUnfocused && document.hasFocus()) {
-                return;
-            }
-
-            const channelId = payload.channelId;
-
-            if (channelId) {
-                ChannelRouter.transitionToChannel(channelId);
-            }
+    flux: {
+        RPC_NOTIFICATION_CREATE({ channelId }) {
+            if (!channelId || settings.store.onlyWhenUnfocused && document.hasFocus()
+                || UserGuildSettingsStore.isGuildOrCategoryOrChannelMuted(ChannelStore.getChannel(channelId)?.guild_id, channelId)) return;
+            ChannelRouter.transitionToChannel(channelId);
         }
-    },
-
-    start() {
-        FluxDispatcher.subscribe(
-            "RPC_NOTIFICATION_CREATE",
-            this.handleNotification,
-        );
-    },
-
-    stop() {
-        FluxDispatcher.unsubscribe(
-            "RPC_NOTIFICATION_CREATE",
-            this.handleNotification,
-        );
     },
 });
